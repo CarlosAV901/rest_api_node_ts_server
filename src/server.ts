f280e19f973e3ede55d481452299c679b0c01687
Charly <aguivega123@hotmail.com>
2024-05-01 14:26:58 -0600

    Se creo el modelo para productos Se creo el middleware para la validacion

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,29 @@
 import express from "express";
+import colors from "colors"
 import router from "./router"
 import db from "./config/db";
 
 //Conectar a base de datos
-async function connecDB() {
+async function connectDB() {
     try {
         await db.authenticate()
         db.sync()
-        console.log("conexion exitosa a la base de datos")
+        console.log(colors.bgGreen("conexion exitosa a la base de datos"))
 
     } catch (error) {
         console.log(error)
-        console.log("Hubo un error al conectar a la base de datos")
+        console.log(colors.red.bold("Hubo un error al conectar a la base de datos"))
     }
 }
 
-connecDB()
+connectDB()
 
+
+//Instancia de express
 const server = express()
+
+//Leer datos de formularios
+server.use(express.json())
 
 server.use('/api/products', router)
 
