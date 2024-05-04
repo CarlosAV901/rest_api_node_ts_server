f4d648a8e0e4854bef5893407a0e19722ab09912
Charly <aguivega123@hotmail.com>
2024-05-03 21:20:13 -0600

    Se agregaron test para cada endpoint

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import router from "./router"
 import db from "./config/db";
 
 //Conectar a base de datos
-async function connectDB() {
+export async function connectDB() {
     try {
         await db.authenticate()
         db.sync()
-        console.log(colors.bgGreen("conexion exitosa a la base de datos"))
+        // console.log(colors.bgGreen("conexion exitosa a la base de datos"))
 
     } catch (error) {
         console.log(error)
@@ -27,6 +27,8 @@ server.use(express.json())
 
 server.use('/api/products', router)
 
-
+server.get('/api', (req, res) => {
+    res.json({ msg: 'desde API' })
+})
 
 export default server
